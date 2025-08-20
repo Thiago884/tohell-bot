6c6526154290da4ae314c344a4d49bdf97d57c4d
Thiago <thiagobarbosa0987@gmail.com>
2025-08-20 15:11:15 -0300

    15.''

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ bot = commands.Bot(
 BOSSES = [
     "Super Red Dragon", "Hell Maine", "Illusion of Kundun",
     "Death Beam Knight", "Genocider", "Phoenix of Darkness",
-    "Hydra", "Rei Kundun", "Erohim"
+    "Hydra", "Rei Kundun"
 ]
 
 # Inicializa apenas com estrutura básica, as salas serão carregadas do banco
